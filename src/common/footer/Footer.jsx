eeb0dcf15f358b5b7124c117c97b06c4b3e83a71
marco <marco_subero1996@hotmail.com>
2021-03-08 20:19:55 -0400

    Aplicación de notas agregada

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -2,11 +2,11 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div id="footer">
+    <footer id="footer">
       <p>&copy; Copyright 2021 - Marco Subero</p>
       <p>Contact: marco_subero1996@hotmail.com</p>
       <p>All Rights Reserved</p>
-    </div>
+    </footer>
   );
 };
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,7 @@
 import "./Portfolio.css";
+
 import project from "../../assets/img/project.png";
+import app_notes from "../../assets/img/app-notes.png";
 
 const Portfolio = () => {
   return (
@@ -8,36 +10,80 @@ const Portfolio = () => {
       <div id="all-projects">
         <div className="project">
           <figure>
-            <img className="project-img" src={project} alt="" />
+            <img className="project-img img-thumbnail" src={project} alt="" />
           </figure>
           <div className="project-info-container">
-            <div>
+            <div className="project-info">
               <h4 className="text-white">QR App</h4>
               <p>Use this QR generator.</p>
+              <div className="button-container">
+                <button>
+                  <a href="/" target="_blank" rel="noreferrer">
+                    <i className="fas fa-external-link-alt"></i> Website
+                  </a>
+                </button>
+                <button>
+                  <a href="/" target="_blank" rel="noreferrer">
+                    <i className="fab fa-github"></i> Repository
+                  </a>
+                </button>
+              </div>
             </div>
           </div>
         </div>
 
         <div className="project">
           <figure>
-            <img className="project-img" src={project} alt="" />
+            <img className="project-img img-thumbnail" src={project} alt="" />
           </figure>
           <div className="project-info-container">
-            <div>
+            <div className="project-info">
               <h4 className="text-white">API Gif</h4>
               <p>Find your favorite gif.</p>
+              <div className="button-container">
+                <button>
+                  <a href="/" target="_blank" rel="noreferrer">
+                    <i className="fas fa-external-link-alt"></i> Website
+                  </a>
+                </button>
+                <button>
+                  <a href="/" target="_blank" rel="noreferrer">
+                    <i className="fab fa-github"></i> Repository
+                  </a>
+                </button>
+              </div>
             </div>
           </div>
         </div>
 
         <div className="project">
           <figure>
-            <img className="project-img" src={project} alt="" />
+            <img className="project-img img-thumbnail" src={app_notes} alt="" />
           </figure>
           <div className="project-info-container">
-            <div>
+            <div className="project-info">
               <h4 className="text-white">Notes App</h4>
               <p>Add, modify and delete your notes.</p>
+              <div className="button-container">
+                <button>
+                  <a
+                    href="https://marco-su.github.io/app-notes/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <i className="fas fa-external-link-alt"></i> Website
+                  </a>
+                </button>
+                <button>
+                  <a
+                    href="https://github.com/Marco-su/app-notes"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <i className="fab fa-github"></i> Repository
+                  </a>
+                </button>
+              </div>
             </div>
           </div>
         </div>
