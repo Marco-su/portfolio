dee4d62cdfdcb78f1742f8db554b9099bda680e0
marco <marco_subero1996@hotmail.com>
2021-03-11 23:51:46 -0400

    Proyecto del clima agregado

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,8 @@
 import "./Portfolio.css";
 
-import project from "../../assets/img/project.png";
 import app_notes from "../../assets/img/app-notes.png";
 import app_qr from "../../assets/img/app-qr.png";
+import app_weather from "../../assets/img/app-weather.png";
 
 const Portfolio = () => {
   return (
@@ -11,7 +11,11 @@ const Portfolio = () => {
       <div id="all-projects">
         <div className="project">
           <figure>
-            <img className="project-img img-thumbnail" src={app_qr} alt="" />
+            <img
+              className="project-img img-thumbnail"
+              src={app_qr}
+              alt="QR-app"
+            />
           </figure>
           <div className="project-info-container">
             <div className="project-info">
@@ -43,20 +47,32 @@ const Portfolio = () => {
 
         <div className="project">
           <figure>
-            <img className="project-img img-thumbnail" src={project} alt="" />
+            <img
+              className="project-img img-thumbnail"
+              src={app_weather}
+              alt="Wheater-app"
+            />
           </figure>
           <div className="project-info-container">
             <div className="project-info">
-              <h4 className="text-white">API Gif</h4>
-              <p>Find your favorite gif.</p>
+              <h4 className="text-white">Weather App</h4>
+              <p>Climate information around the world.</p>
               <div className="button-container">
                 <button>
-                  <a href="/" target="_blank" rel="noreferrer">
+                  <a
+                    href="https://marco-su.github.io/app-weather/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <i className="fas fa-external-link-alt"></i> Website
                   </a>
                 </button>
                 <button>
-                  <a href="/" target="_blank" rel="noreferrer">
+                  <a
+                    href="https://github.com/Marco-su/app-weather"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <i className="fab fa-github"></i> Repository
                   </a>
                 </button>
@@ -67,7 +83,11 @@ const Portfolio = () => {
 
         <div className="project">
           <figure>
-            <img className="project-img img-thumbnail" src={app_notes} alt="" />
+            <img
+              className="project-img img-thumbnail"
+              src={app_notes}
+              alt="Notes-app"
+            />
           </figure>
           <div className="project-info-container">
             <div className="project-info">
